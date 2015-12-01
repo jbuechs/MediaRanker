5e181e46bba95615cfd2e5c1e432f2d2acf865d9
Jennie Buechner <janina.buechner@gmail.com>
2015-12-01 14:21:08 -0800

    add some tests for movies controller

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -8,19 +8,32 @@ RSpec.describe MoviesController, type: :controller do
     end
   end
 
-  describe "POST 'create'" do
-    
-  end
-
   describe "GET 'new'" do
-
+    it "renders new view" do
+      get :new
+      expect(subject).to render_template :new
+    end
   end
 
-  describe "GET 'edit'" do
-
-  end
+  # describe "GET 'edit'" do
+  #   it "renders edit view" do
+  #     movie = Movie.first
+  #     get :edit, movie
+  #     expect(subject).to render_template :edit, movie
+  #   end
+  # end
 
   describe "GET 'show'" do
+    let(:new_movie) do
+      Movie.create(title:"Schindler's List 2", director: "Steven Spielberg")
+    end
+    it "renders show view" do
+      get :show, id: new_movie.id
+      expect(subject).to render_template :show
+    end
+  end
+
+  describe "POST 'create'" do
 
   end
 
