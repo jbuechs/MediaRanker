5b744c95f17f3ddfb9e91602b5b3a6b5f6892172
Jennie Buechner <janina.buechner@gmail.com>
2015-12-01 14:22:42 -0800

    add empty methods to albums and books controller tests

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -7,4 +7,33 @@ RSpec.describe BooksController, type: :controller do
       expect(response.status).to eq 200
     end
   end
+  describe "GET 'new'" do
+    it "renders new view" do
+      get :new
+      expect(subject).to render_template :new
+    end
+  end
+
+  describe "GET 'edit'" do
+  end
+
+  describe "GET 'show'" do
+
+  end
+
+  describe "POST 'create'" do
+
+  end
+
+  describe "PATCH 'update'" do
+
+  end
+
+  describe "DELETE 'destroy'" do
+
+  end
+
+  describe "POST 'upvote'" do
+
+  end
 end
