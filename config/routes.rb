b12ec593bdaf1aa39f81b04137603bbb318492db
Jennie Buechner <janina.buechner@gmail.com>
2015-11-30 21:27:09 -0800

    create upvotes for albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
   root 'homepage#index'
-  resources :movies
-  resources :books
-  resources :albums
+  resources :movies do
+    member do
+      post 'upvote'
+    end
+  end
+  resources :books do
+    member do
+      post 'upvote'
+    end
+  end
+  resources :albums do
+    member do
+      post 'upvote'
+    end
+  end
 end
