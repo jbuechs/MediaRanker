155f8ea11dd6d50f0c4214d5db29a721346064e8
Jennie Buechner <janina.buechner@gmail.com>
2015-11-30 16:18:59 -0800

    create index for all views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,10 @@
 class BooksController < ApplicationController
+  before_action :get_book only:[:show, :destroy, :update, :edit]
+  def index
+  end
+
+  private
+  def get_book
+    @book = Book.find(params[:id])
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+  before_action :get_movie only:[:show, :destroy, :update, :edit]
+
+  def index
+  end
+
+  private
+  def get_movie
+    @movie = Movie.find(params[:id])
+  end
 end
