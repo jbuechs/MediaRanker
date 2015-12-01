1ca59ec6860e268b9582e18c2cb1c08d15e40530
Jennie Buechner <janina.buechner@gmail.com>
2015-11-30 16:18:59 -0800

    create index for all views

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,11 @@
 class AlbumsController < ApplicationController
+  before_action :get_album only:[:show, :destroy, :update, :edit]
+  def index
+
+  end
+
+  private
+  def get_album
+    @album = Album.find(params[:id])
+  end
 end
