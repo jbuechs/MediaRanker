3960870fc2cf5de216a6031ce9d364143b063e26
Jennie Buechner <janina.buechner@gmail.com>
2015-12-02 08:23:28 -0800

    create test for homepage

--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe HomepageController, type: :controller do
   describe "GET 'index'" do
-    
+    it "renders the index template" do
+      get :index
+      expect(subject).to render_template :index
+    end
   end
 
 end
